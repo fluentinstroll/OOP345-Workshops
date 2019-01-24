17e2962dc429d1dd9f586612a722825d23c6d95d
Ray <32021785+fluentinstroll@users.noreply.github.com>
2019-01-24 10:22:27 -0500

    Removed personal information

--- WS1/process.cpp
+++ WS1/process.cpp
@@ -1,11 +1,7 @@
-/* Author: Raymond Rambo (Student Number)
-Date Created: 13/09/2018
-Change Log:
-*/
 #include <ostream>
 #include "process.h"
 #include "CString.h"
 void process(char *input, std::ostream& os) {
-	w1::CString(*input); // figure out how to make this into a new object (probably requires finishing the constructor)
+	w1::CString(*input);
 	os << newString; // inserting into ostream object
 }
